ce7e4ecc93c8197040fc5c5ea82e9508b6d423fe
Nguyen Khanh Duy <trdbau@gmail.com>
2024-10-10 16:45:19 +0700

    update search bar

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
+import { BookMarked, User } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import VocabularySearch from '@/components/ui/vocabularyInput'
 
 import { DictionaryProps } from '@/types/common'
 
 function Header({ dictionary }: DictionaryProps) {
   return (
-    <header className="container sticky top-0 z-10 flex items-center justify-between bg-neutral-3 py-4">
-      <span>LOGO</span>
+    <header className="bg-background w-full border-b">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 py-4 md:flex-row">
+        <Link href="/" className="flex items-center space-x-2">
+          <BookMarked />
+          <span className="text-xl font-bold">{dictionary['Learn English']}</span>
+        </Link>
 
-      {dictionary['Contact Us']}
+        <div className="mx-4 w-full md:w-auto md:max-w-md md:flex-1">
+          <form className="relative">
+            <VocabularySearch />
+          </form>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <Button variant="ghost" className="hidden md:flex">
+            {dictionary['Sign Up']}
+          </Button>
+          <Button>
+            <User className="mr-2 h-4 w-4" />
+            {dictionary['Log In']}
+          </Button>
+        </div>
+      </div>
     </header>
   )
 }
