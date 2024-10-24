f5612dd60beb9bc2ec85f27f81eaf645c8e8ce59
Bac Nguyen <trdbau@gmail.com>
2024-10-24 19:33:12 +0700

    feat(audio hook): [11] implement useAudioRecorder hook

---
+++ src/components/hook/use-audio-recorder.ts
@@ -0,0 +1,46 @@
+import { useRef, useState } from 'react'
+
+export function useAudioRecorder() {
+  const [isRecording, setIsRecording] = useState(false)
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null)
+  const audioChunksRef = useRef<Blob[]>([])
+  const [audioUrl, setAudioUrl] = useState<string | null>(null)
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null)
+
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+      mediaRecorderRef.current = new MediaRecorder(stream)
+
+      mediaRecorderRef.current.ondataavailable = (ev) => {
+        audioChunksRef.current.push(ev.data)
+      }
+
+      mediaRecorderRef.current.onstop = () => {
+        const blob = new Blob(audioChunksRef.current, { type: 'audio/wav' })
+        setAudioBlob(blob)
+        const url = URL.createObjectURL(blob)
+        setAudioUrl(url)
+        audioChunksRef.current = []
+      }
+
+      mediaRecorderRef.current.start()
+      setIsRecording(true)
+    } catch (error) {
+      console.error('Error accessing microphone', error)
+    }
+  }
+
+  const stopRecording = () => {
+    mediaRecorderRef.current?.stop()
+    setIsRecording(false)
+  }
+
+  return {
+    isRecording,
+    audioUrl,
+    audioBlob,
+    startRecording,
+    stopRecording,
+  }
+}
