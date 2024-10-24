1b42a97d0afa1879bb97d7d2bb1b202cee53f4d6
Bac Nguyen <trdbau@gmail.com>
2024-10-24 19:48:07 +0700

    feat(check-phonemes): add check-phonemes api

---
+++ src/services/csr/phonemes.ts
@@ -0,0 +1,27 @@
+import { AxiosError } from 'axios'
+
+import ENDPOINTS from '../end-points'
+import axiosClient from '..'
+
+const PhonemesService = {
+  async check(audioFile: Blob, groundTruth: string): Promise<any> {
+    const formData = new FormData()
+    formData.append('audio_file', audioFile)
+    formData.append('ground_truth', groundTruth)
+
+    return axiosClient
+      .post<any>(ENDPOINTS.PHONEMES.CHECK, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        return res.data
+      })
+      .catch((error: AxiosError<any>) => {
+        return error.response?.data
+      })
+  },
+}
+
+export default PhonemesService
