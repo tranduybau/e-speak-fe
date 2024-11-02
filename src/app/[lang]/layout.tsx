fa493d36cfaa17311a2d0221e9623660cf61de96
hoang cong minh <0782708546hoangcongminh@gmail.com>
2024-11-03 06:12:15 +0700

    [sidebar] build sidebar ui

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,3 +1,7 @@
+import { Separator } from '@/components/ui/separator'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+
+import { AppSidebar } from '@/components/app-sidebar'
 import { getDictionary } from '@/dictionaries/get-dictionary'
 import Footer from '@/layouts/footer'
 import Header from '@/layouts/header'
@@ -11,11 +15,20 @@ async function Layout({ children, params }: Props) {
   const dictionary = await getDictionary(params.lang)
   return (
     <>
-      <Header dictionary={dictionary} />
-
-      <div className="flex flex-1 flex-col">{children}</div>
-
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
       <Footer dictionary={dictionary} />
+      <Header dictionary={dictionary} />
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,22 @@ const config = {
     './src/**/*.{ts,tsx}',
   ],
   prefix: '',
-  theme: {},
+  theme: {
+  	extend: {
+  		colors: {
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		}
+  	}
+  },
   // eslint-disable-next-line global-require
   plugins: [require('tailwindcss-animate')],
 } satisfies Config
